9c0b11b23d88d66fcd30e8caf269631b4f9b18d9
Ankit <codewithankit047@gmail.com.com>
2024-02-22 15:10:41 +0530

    get api

---
+++ data.js
@@ -0,0 +1,9 @@
+// In data.js
+const data = [{ name: "Ankit", email: "codewithankit047@gmail.com", mobile: "8930395398" },
+{ name: "Ankit", email: "codewithankit047@gmail.com", mobile: "8930395398" },
+{ name: "Ankit", email: "codewithankit047@gmail.com", mobile: "8930395398" },
+{ name: "Ankit", email: "codewithankit047@gmail.com", mobile: "8930395398" },
+{ name: "Ankit", email: "codewithankit047@gmail.com", mobile: "8930395398" },
+];
+
+module.exports = data;
