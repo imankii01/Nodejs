3b8aa42ad92b7ddd740047f9a09eda8ce285109f
Ankit <codewithankit047@gmail.com.com>
2024-02-22 16:39:55 +0530

    status update

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const http = require('http');
 const express = require('express');
-const data = require('./data');
-const app=express();
+const app = express();
 
-// http.createServer((req, resp) => {
-//     resp.writeHead(200, { 'Content-Type': 'application\json' });
-//     resp.write(JSON.stringify(data));
-//     resp.end();
-// }).listen(7000)
+app.get('/:name', (req, resp) => {
+    resp.send(req?.params)
+    resp.send("<h1>Home page....</h1>");
+});
 
+app.get('/about', (req, resp) => {
+    resp.send("<h1>About page....</h1>");
+});
 
-app.get('',(req,resp)=>{
-    resp.send("<h1>Home page....</h1>")
-})
-app.get('/about',(req,resp)=>{
-    resp.send("<h1>About page....</h1>")
-})
-app.get('/login',(req,resp)=>{
-    resp.send("<h1>login page....</h1>")
-})
-app.get('/contact',(req,resp)=>{
-    resp.send("<h1>Contact page....</h1>")
-})
-app.listen(7000)
+app.get('/login', (req, resp) => {
+    resp.send("<h1>Login page....</h1>");
+});
+
+app.get('/contact', (req, resp) => {
+    resp.send("<h1>Contact page....</h1>");
+});
+
+// Example route with a parameter
+app.get('/user/:name', (req, resp) => {
+    console.log(req.params.name);
+    resp.send(`<h1>Hello, ${req.params.name}!</h1>`);
+});
+
+app.listen(7000, () => {
+    console.log('Server is running on port 7000');
+});
