909c47295814832073902c3f3dbec5de50d200ff
Ankit <codewithankit047@gmail.com.com>
2024-02-22 14:25:51 +0530

    update

--- Begining/index.jsx
+++ Begining/index.jsx
@@ -1,1 +1,8 @@
-console.log("hello world")
+// console.log("hello world")
+// var  a= 30;
+// var b = 40
+// console.warn(a+b)
+// var x = 20
+// for (i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
